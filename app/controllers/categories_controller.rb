8bf9e74ac2054254e83a03eb40b211a1aac5bb7a
Nakai <nakai.hamlin18@gmail.com>
2021-05-04 00:54:45 -0400

    commit tree 42e69192fbca86a1b5bfb6dbeefe8a8b4c5988aa

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,33 +2,23 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
-
   def show
     @category = Category.find(params[:id])
   end
-
   def new
     @category = Category.new
   end
-
   def create
-    category = Category.create(category_params)
+    category = Category.create(params[:category])
     redirect_to category_path(category)
   end
-
   def edit
     @category = Category.find(params[:id])
   end
 
   def update
     category = Category.find(params[:id])
-    category.update(category_params)
+    category.update(params.require(:category).permit(:name))
     redirect_to category_path(category)
-  end
-
-  private
-
-  def category_params
-    params.require(:category).permit(:name)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,35 +2,25 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
-
 	def show
 		@post = Post.find(params[:id])
 	end
-
 	def new
 		@post = Post.new
 		@categories = Category.all
 	end
-
 	def create
-		post = Post.create(post_params)
+		post = Post.create(params[:post])
 		redirect_to post_path(post)
 	end
-
 	def edit
 		@post = Post.find(params[:id])
 		@categories = Category.all
 	end
 
 	def update
-	  post = Post.find(params[:id])
-	  post.update(post_params)
-	  redirect_to post_path(post)
+		post = Post.find(params[:id])
+		post.update(params.require(:post).permit(:title,:description))
+		redirect_to post_path(post)
 	end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
 end
